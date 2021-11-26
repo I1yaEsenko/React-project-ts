68b4d8bf9fe0787e9503e5cb717d9d1c3ac8f226
I1yaEsenko <ilya.esenko@gmail.com>
2021-11-26 22:42:31 +0300

    Update App.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,33 @@
 import React from 'react';
 import './App.css';
+import {Rating} from "./components/Rating/Rating";
+import {Accordion} from "./components/Accordion/Accordion";
 
 function App() {
   console.log("App rendering");
   return (
     <div>
-      <AppTitle/>
-      <Rating/>
-      <Accordion/>
-      <Star/>
+      <AppTitle title={"This is TS App"}/>
+      <Rating value={1}/>
+      <Accordion title={"Меню правое"} collapsed={true}/>
+      <Accordion title={"Меню левое"} collapsed={false}/>
+      <Rating value={0}/>
+      <Rating value={1}/>
+      <Rating value={2}/>
+      <Rating value={3}/>
+      <Rating value={4}/>
+      <Rating value={5}/>
     </div>
   );
 }
-function AppTitle(){
+
+type AppTitlePropsType = {
+  title: string
+}
+
+function AppTitle(props: AppTitlePropsType){
   console.log("AppTitle rendering");
-  return <>This is App component</>
-}
-function Rating() {
-  console.log("Rating rendering");
-  return (
-    <div>
-      <Star/>
-      <Star/>
-      <Star/>
-      <Star/>
-      <Star/>
-    </div>
-  );
-}
-
-function Accordion() {
-  console.log("Accordion rendering");
-  return (
-    <div>
-      <AccordionTitle/>
-      <AccordionBody/>
-    </div>
-  );
-}
-
-function Star() {
-  console.log("Star rendering");
-  return <div>star</div>
-
-}
-
-function AccordionTitle() {
-  console.log("AccordionTitle rendering");
-  return <h3>Menu</h3>
-}
-
-function AccordionBody() {
-  console.log("AccordionBody rendering");
-  return (
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </ul>
-  );
+  return <h1>{props.title}</h1>
 }
 
 
