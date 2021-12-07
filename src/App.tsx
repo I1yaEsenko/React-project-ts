a0c556878d2c9af5abb44e9751189a2aa27ee437
Ilya Esenko <88851125+I1yaEsenko@users.noreply.github.com>
2021-12-07 08:55:56 +0300

    add components and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
 import React from 'react';
 import './App.css';
-import {Rating} from "./components/Rating/Rating";
-import {Accordion} from "./components/Accordion/Accordion";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Profile from "./components/Profile/Profile";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  console.log("App rendering");
   return (
     <div>
-      <AppTitle title={"This is TS App"}/>
-      <Rating value={1}/>
-      <Accordion title={"Меню правое"} collapsed={true}/>
-      <Accordion title={"Меню левое"} collapsed={false}/>
-      <Rating value={0}/>
-      <Rating value={1}/>
-      <Rating value={2}/>
-      <Rating value={3}/>
-      <Rating value={4}/>
-      <Rating value={5}/>
+      <div className='app-wrapper'>
+        <Header/>
+        <Sidebar/>
+        <Profile/>
+        <Footer/>
+      </div>
     </div>
   );
 }
 
-type AppTitlePropsType = {
-  title: string
-}
-
-function AppTitle(props: AppTitlePropsType){
-  console.log("AppTitle rendering");
-  return <h1>{props.title}</h1>
-}
 
 
 export default App;
